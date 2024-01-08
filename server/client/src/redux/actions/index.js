08b20cced0912ff91b94111556e92fb249608fa1
Zaid627 <154814781+Zaid627@users.noreply.github.com>
2024-01-08 23:43:45 +0500

    Update index.js

--- server/client/src/redux/actions/index.js
+++ server/client/src/redux/actions/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { ADDNEW_TODO, GETALL_TODO, TOGGLE_TODO, UPDATE_TODO,  DELETE_TODO, TOGGLE_TAB } from './type';
 
-const API_URL= 'http://localhost:8000';
+const API_URL= 'https://mern-todo-uzum.vercel.app';
 
 export const addNewTodo = (data) =>async(dispatch)=>{
     try {
